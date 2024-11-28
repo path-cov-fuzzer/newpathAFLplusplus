3a5e97ac9ea2430e099a2c43dcc9ce9144b6f78a
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-11-29 07:50:25 +0800

    seems finish filterCallmap.py

--- fuzzing_support/filterCallmap.py
+++ fuzzing_support/filterCallmap.py
@@ -1,40 +1,46 @@
 import sys
 import re
 
-# 检查是否传入了足够的参数（至少需要三个参数加上脚本名称）
 assert(len(sys.argv) == 4)
-# 打印第一个、第二个和第三个参数
+
 # python3 filterCallmap.py cfg_filtered.txt callmap.txt callmap_filtered.txt
-# print(f"参数1: {sys.argv[1]}")
-# print(f"参数2: {sys.argv[2]}")
-# print(f"参数3: {sys.argv[3]}")
+print(f"arg1: {sys.argv[1]}")
+print(f"arg2: {sys.argv[2]}")
+print(f"arg3: {sys.argv[3]}")
 
-# 思考代码：
-# 1.读取 cfg_filtered.txt，扫描每个 BasicBlock，记录它们的编号，存到一个字典里
-# 2.读取 callmap.txt 的每一行，根据读到的 BBID 去比较字典里是否存在这个 BBID，来决定是否把这行输出到 callmap_filtered.txt 里
+# The approach for filtering the Callmap.
+# 1. reading cfg_filtered.txt, scan each BasicBlock's BBID into a dict
+# 2. scan each line of callmap.txt, if BBID of this line exits in dict, then dump this line to callmap_filtered.txt
 
-# 1.读取 cfg_filtered.txt，扫描每个 BasicBlock，记录它们的编号，存到一个字典里
+# 1. reading cfg_filtered.txt, scan each BasicBlock's BBID into a dict
 bbdict = {}
 with open(sys.argv[1], 'r', encoding='utf-8') as file:
+    # read each line
     for line in file:
-        # 使用 strip() 去除每行末尾的换行符
+        # remove leading/trailing whitespaces
         line = line.strip()
         BasicBlock_match = re.search(r'BasicBlock: (\d+)', line)
         if BasicBlock_match:
+            # if this line is "BasicBlock: integer", then record this integer into dict
             bbid = int(BasicBlock_match.group(1))
             bbdict[bbid] = 1
         else:
-            # 如果这行不包含 "BasicBlock"，那么不做任何事情
+            # if not, skip this line
             pass
 
-# 2.读取 callmap.txt 的每一行，根据读到的 BBID 去比较字典里是否存在这个 BBID，来决定是否把这行输出到 callmap_filtered.txt 里
+# 2. scan each line of callmap.txt, if BBID of this line exits in dict, then dump this line to callmap_filtered.txt
+# open writting privilege of "callmap_filtered.txt"
 written_file = open(sys.argv[3], 'w')
+# open reading privilege of "callmap.txt"
 with open(sys.argv[2], 'r', encoding='utf-8') as file:
+    # read each line of "callmap.txt"
     for line in file:
-        # 使用 strip() 去除每行末尾的换行符
+        # remove leading/trailing whitespaces
         line = line.strip()
+        # parse the BBID of this line
         bbid_match = re.search(r'(\d+).*', line)
         bbid = int(bbid_match.group(1))
+        # if BBID exits in bbdict, then dump this line to "callmap_filtered.txt"
         if bbid in bbdict:
             written_file.write(line + "\n")
 
