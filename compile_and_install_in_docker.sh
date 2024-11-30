61c77bd26577b0b6bcbafbc418a397cbd02dfc7a
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-11-30 17:50:57 +0800

    fix compile_and_install_in_docker.sh

--- compile_and_install_in_docker.sh
+++ compile_and_install_in_docker.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
-LLVM_CONFIG=llvm-config-17 make -e -j$(nproc)
+# generate libhashcompare.a static library
+clang++ -c hashcompare.cpp
+ar rcs libhashcompare.a hashcompare.o
 
+# -lcrypto: use <openssl/sha.h>
+# -lhashcompare: use hashcompare.cpp
+# -lstdc++: when use gcc/clang link .o files. If some .o files is compiled from C++, then -lstdc++ is necessary to link these files.
+LLVM_CONFIG=llvm-config-17 LDFLAGS="-L$(pwd) -lcrypto -lhashcompare -lstdc++" make -e -j$(nproc)
+
+# aflpp_driver is not changed, so its compilation process does not have to change
 export CC=clang
 export CXX=clang++
 export AFL_NO_X86=1
