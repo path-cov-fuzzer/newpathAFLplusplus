cb4195a86c4c31c0a42cce7163b2c62c0521469a
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-13 19:08:37 +0800

    fix compile_and_install_in_docker.sh

--- compile_and_install_in_docker.sh
+++ compile_and_install_in_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd $FUZZER/repo
+
 LLVM_CONFIG=llvm-config-17 make -e -j$(nproc)
 
 export CC=clang
@@ -7,4 +9,6 @@ export CXX=clang++
 export AFL_NO_X86=1
 export PYTHON_INCLUDE=/
 LLVM_CONFIG=llvm-config-17 make -e -C utils/aflpp_driver || exit 1
+
+popd
 
