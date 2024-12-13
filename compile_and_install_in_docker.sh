69ed201dc5126f0b51098cdbb7f2985c52fb1da2
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-13 21:30:33 +0800

    fix compile_and_install_in_docker.sh

--- compile_and_install_in_docker.sh
+++ compile_and_install_in_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+pushd $FUZZER/repo
+
 # generate libhashcompare.a static library
 clang++ -c hashcompare.cpp
 ar rcs libhashcompare.a hashcompare.o
@@ -15,4 +17,6 @@ export CXX=clang++
 export AFL_NO_X86=1
 export PYTHON_INCLUDE=/
 LLVM_CONFIG=llvm-config-17 make -e -C utils/aflpp_driver || exit 1
+
+popd
 
