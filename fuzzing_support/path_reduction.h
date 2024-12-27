25d1b1264e27d9b60d9f4cc380ba84e89ec626a1
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2024-12-27 16:41:43 +0800

    add commented debug code, and fix free_boxed_array problem

--- fuzzing_support/path_reduction.h
+++ fuzzing_support/path_reduction.h
@@ -2,16 +2,19 @@
 #define PATH_REDUCER_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef struct PathReducer PathReducer;
-typedef uint32_t BlockID;
-typedef uint32_t FunID;
+typedef int32_t BlockID;
+typedef int32_t FunID;
 
 // Gets a path reducer
 PathReducer* get_path_reducer(const void* top_level, int32_t k);
 
 // Frees a path reducer
 void free_path_reducer(PathReducer* ptr);
+
+void free_boxed_array(int* ptr, size_t len);
 
 // Reduces a path, and gets the hash
 const char* reduce_path(const PathReducer* reducer, const BlockID* path, int32_t path_size, FunID entry_fun_id);
@@ -20,5 +23,4 @@ const char* reduce_path(const PathReducer* reducer, const BlockID* path, int32_t
 const BlockID* reduce_path1(const PathReducer* reducer, const BlockID* path, int32_t path_size, FunID entry_fun_id, int* out_len);
 
 #endif
-
 
