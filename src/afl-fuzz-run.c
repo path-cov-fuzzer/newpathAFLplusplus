03b6a2d119a89ef145cb29db5ac8f2a4ad40d499
YinhuaChen-cloud <11612127@mail.sustech.edu.cn>
2025-01-03 18:59:09 +0800

    change to assert(afl->fsrv.path_trace_bits[0] >= 0);

--- src/afl-fuzz-run.c
+++ src/afl-fuzz-run.c
@@ -1245,11 +1245,19 @@ u8 __attribute__((hot)) common_fuzz_stuff(afl_state_t *afl, u8 *out_buf,
     // points to the reduced path
     BlockID *reduced_path = NULL;
 
-    // path length must be greater than 0 
-    if (afl->fsrv.path_trace_bits[0] <= 0) {
-        printf("cyherr: afl->fsrv.path_trace_bits[0] = %u\n", afl->fsrv.path_trace_bits[0]);
-        assert(0);
-    }
+    // // path length must be greater than 0 
+    // if (afl->fsrv.path_trace_bits[0] <= 0) {
+    //     printf("cyherr: afl->fsrv.path_trace_bits[0] = %u\n", afl->fsrv.path_trace_bits[0]);
+    //     printf("========== out_buf start ============\n");
+    //     const unsigned char *byte = (const unsigned char *)out_buf; // 转换为字节指针
+    //     for (size_t i = 0; i < len; i++) {
+    //         printf("%c", byte[i]); // 打印每个字节为字符
+    //     }
+    //     printf("\n");
+    //     printf("========== out_buf end ============\n");
+    //     assert(0);
+    // }
+    assert(afl->fsrv.path_trace_bits[0] >= 0);
 
     // get u32* pointer of the path beginning
     u32 *path_shm_ptr_to_1 = &(afl->fsrv.path_trace_bits[1]);   
